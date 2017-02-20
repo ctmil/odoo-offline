c73583ca329847e21b0a71eb8fa6ceb98bebb285
morrillo <gustavo.orrillo@gmail.com>
2017-02-20 16:07:01 -0300

    Added point of sale and new id to ticket

--- odoo-app/src/app/tickets.ts
+++ odoo-app/src/app/tickets.ts
@@ -2,13 +2,15 @@ import { TicketItem } from "./ticket-item";
 import { TicketItems } from "./ticket-items";
 
 export class Tickets {
-    id: number;
+    id: string = '';
     date: Date;
     //amount: number = 0;
     client: string = '';
     seller: string = '';
     //complete: boolean = false;
     items: TicketItems = [];
+    // error_message
+    //error_message: string = '';
 
     //private _items_total : number = 0;
     constructor(values: Object = {}) {
@@ -20,7 +22,7 @@ export class Tickets {
       var _items_total : number = 0;
       for (var itemid in this.items) {
         var Item: TicketItem = this.items[itemid];
-        if (Number(Item.product_qty)>0 && Number(Item.product_qty)>0)
+        if (Number(Item.product_qty)>0 && Number(Item.product_unit_price)>0)
           _items_total += Number(Item.product_qty) * Number(Item.product_unit_price);
       }
       return _items_total;
@@ -28,23 +30,32 @@ export class Tickets {
 
     isValid(): boolean {
       if (this.date == null) {
+        //this.error_message = 'Debe ingresarse la fecha';
         return false;
       }
-      console.log('isValid');
-      console.log(this.date);
       // Validates date is greater than yesterday
       var yesterday = new Date();
       yesterday.setDate(yesterday.getDate() - 1);
-      console.log(yesterday);
       if (this.date <= yesterday) {
+        //this.error_message = 'La fecha debe ser el día de hoy';
         return false;
       }
       if (this.client == null) {
+        //this.error_message = 'Debe ingresarse el cliente';
         return false;
       }
       if (this.items == null || this.items.length == 0) {
+        //this.error_message = 'Debe ingresarse al menos un producto';
         return false;
       }
+      for (var itemid in this.items) {
+        var Item: TicketItem = this.items[itemid];
+        if (Number(Item.product_qty) <= 0 || Number(Item.product_unit_price) <= 0) {
+          //this.error_message = 'El importe o cantidad debe ser mayor a 0';
+          return false;
+          }
+        }
+
       return true;
     }
 
